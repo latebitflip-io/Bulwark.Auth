a86a9a933460ae0ec2dd5c2ab6919289fd2b54ae
Fritz (Fredrick Seitz) <fredrick.seitz@gmail.com>
2023-07-27 14:54:39 -0400

    fix: remove uneeded property

--- src/Bulwark.Auth/Common/Payloads/Awknowledge.cs
+++ src/Bulwark.Auth/Common/Payloads/Awknowledge.cs
@@ -4,7 +4,6 @@ public class Acknowledge
     public string Email { get; set; }
 	public string DeviceId { get; set; }
     public string AccessToken { get; set; }
-    public string IdToken { get; set; }
     public string RefreshToken { get; set; }
 }
 
